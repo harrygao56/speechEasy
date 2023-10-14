9b06310657e43716177248a29df9023160561d42
ryan38538 <70866983+ryan38538@users.noreply.github.com>
2023-10-14 14:30:30 -0500

    Brand added

---
+++ react-flask-app/src/components/Brand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import '/Brand.css'
+const Brand = () => {
+    return (
+        <div className="box">
+          <p className="box-text">SpeechEasy</p>
+        </div>
+    );
+};
+
+export default Brand;
